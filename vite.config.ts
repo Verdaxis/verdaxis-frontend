f03ca017832a3b2db4bfc130d29f4ce10e2e2836
Verdaxis Prod <verdaxis-prod@verdaxis.exchange>
2026-09-07 13:14:55 +0800

    Revert "Revert "fix: pass build command to staging map token validation""
    This reverts commit d897faca9834329b7f65fdc912ec841bf22d790e.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, '.', '');
     if (command === 'build' && ['production', 'staging'].includes(mode)) {
         const mapToken = process.env.VITE_MAPBOX_PUBLIC_TOKEN ?? env.VITE_MAPBOX_PUBLIC_TOKEN;
